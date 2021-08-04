14a9ea4cddf10d5e63b7b8a4a2d93aeed98e271c
Anukul Joshi <42417337+anukuljoshi@users.noreply.github.com>
2021-08-04 13:43:58 +0530

    lesson-7

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,12 +1,41 @@
-import { makeStyles, Typography, Button, Container } from "@material-ui/core";
+import React, { useState } from "react";
 
+import { makeStyles, Typography, Button, Container, TextField } from "@material-ui/core";
 import { KeyboardArrowRight } from '@material-ui/icons'
 
 const useStyles = makeStyles({
+	field: {
+		marginTop: 20,
+		marginBottom: 20,
+		display: 'block',
+	}
 })
 
 const Create = () => {
 	const classes = useStyles();
+
+	const [title, setTitle] = useState('');
+	const [details, setDetails] = useState('');
+	
+	const [titleError, setTitleError] = useState(false);
+	const [detailsError, setDetailsError] = useState(false);
+
+	const handleSubmit = (event: React.FormEvent) => {
+		event.preventDefault();
+
+		setTitleError(false);
+		setDetailsError(false);
+
+		if(title===''){
+			setTitleError(true);
+		}
+		if(details===''){
+			setDetailsError(true);
+		}
+		if(title && details){
+			console.log(title, details)
+		}
+	}
 
 	return (
 		<Container>
@@ -19,15 +48,39 @@ const Create = () => {
 				Create a New Node
 			</Typography>
 
-			<Button
-				onClick={() => console.log('Button clicked')}
-				type="submit"
-				color="secondary"
-				variant="contained"
-				endIcon={<KeyboardArrowRight></KeyboardArrowRight>}
-			>
-				Submit
-			</Button>
+			<form noValidate autoComplete={'off'} onSubmit={handleSubmit}>
+				<TextField 
+					onChange={(e) => setTitle(e.target.value)}
+					className={classes.field}
+					label="Notes Title"
+					variant="outlined"
+					color="secondary"
+					fullWidth
+					required
+					error={titleError}
+				></TextField>
+				<TextField 
+					onChange={(e) => setDetails(e.target.value)}
+					className={classes.field}
+					label="Details"
+					variant="outlined"
+					color="secondary"
+					fullWidth
+					required
+					multiline
+					rows={4}
+					error={detailsError}
+				></TextField>
+				<Button
+					type="submit"
+					color="secondary"
+					variant="contained"
+					endIcon={<KeyboardArrowRight></KeyboardArrowRight>}
+				>
+					Submit
+				</Button>
+			</form>
+
 		</Container>
 	);
 };
