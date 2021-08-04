d8fdc8d6ffce5dae7e39e950517865772818b5c3
Anukul Joshi <42417337+anukuljoshi@users.noreply.github.com>
2021-08-04 12:14:38 +0530

    lesson 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Notes from "./pages/Notes";
+import Create from "./pages/Create";
 
 function App() {
 	return (
-		<div>
-			<h1>Hello World</h1>
-		</div>
+		<Router>
+			<Switch>
+				<Route exact path={"/"}>
+					<Notes></Notes>
+				</Route>
+				<Route exact path={"/create"}>
+					<Create></Create>
+				</Route>
+			</Switch>
+		</Router>
 	);
 }
 
